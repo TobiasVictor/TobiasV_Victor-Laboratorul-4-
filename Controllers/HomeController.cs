610e177e7f52ab53882b45a177bd166d1f88cd83
Tobias Victor <bboyvictor17@yahoo.com>
2020-11-30 12:49:56 +0200

    Am terminat lab 7

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,36 +12,15 @@ using TobiasV_Victor_Lab2.Models.LibraryViewModels;
 
 namespace TobiasV_Victor_Lab2.Controllers
 {
+
     public class HomeController : Controller
     {
         private readonly LibraryContext _context;
-
         public HomeController(LibraryContext context)
         {
             _context = context;
         }
 
-        
-        public async Task<ActionResult> Statistics()
-        {
-            IQueryable<OrderGroup> data =
-                from order in _context.Orders
-                group order by order.OrderDate into dateGroup
-                select new OrderGroup()
-                {
-                    OrderDate = dateGroup.Key,
-                    BookCount = dateGroup.Count()
-
-                };
-            return View(await data.AsNoTracking().ToListAsync());
-        }
-
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
@@ -57,6 +36,19 @@ namespace TobiasV_Victor_Lab2.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<ActionResult> Statistics()
+        {
+            IQueryable<OrderGroup> data =
+            from order in _context.Orders
+            group order by order.OrderDate into dateGroup
+            select new OrderGroup()
+            {
+                OrderDate = dateGroup.Key,
+                BookCount = dateGroup.Count()
+            };
+            return View(await data.AsNoTracking().ToListAsync());
         }
     }
 }
